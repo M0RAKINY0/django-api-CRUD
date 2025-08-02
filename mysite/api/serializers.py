eb3500bce74118645e64877776dfb28b92e08f7a
morakinyo <morakinyokolade43@gmail.com>
2025-08-02 12:22:55 +0100

    project done

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -1,8 +1,7 @@
-#json = js object notation
 from rest_framework import serializers
 from .models import BlogPost
 
 class BlogPostSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = BlogPost
         fields = ["id", "title", "content", "published_date"]
--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("blogPost/", views.BlogPostListCreate.as_view(),name="blogpost-view-create"),
+    path("blogPost/<int:pk>/", views.BlogpostRetrieveUpdateDestroy.as_view(), name="update"),
 ]
