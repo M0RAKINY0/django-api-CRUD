0f5309b81d389b4b2026897dd34c5d7a5a63436f
morakinyo <morakinyokolade43@gmail.com>
2025-08-02 10:27:33 +0100

    Update models.py

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
-    title = models.charField(max_length = 100)
+    title = models.CharField(max_length = 100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add = True)
 
