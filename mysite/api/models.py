6fe3fe510465656300aba288c8cfb42472ef2c28
morakinyo <morakinyokolade43@gmail.com>
2025-08-05 11:15:12 +0100

    trying to integrate db and making small refinement to model.py

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -5,7 +5,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add = True)
-
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title 
